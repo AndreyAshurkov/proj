c1084f8217a3799f555b98e1eb9bb148a30cf792
AndreyAshurkov <48838719+AndreyAshurkov@users.noreply.github.com>
2019-06-04 00:49:01 +0300

    fix

--- model/airline.py
+++ model/airline.py
@@ -5,8 +5,4 @@ class Airline(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(30), unique=True, nullable=False)
 
-    def get_username(self):
-        return self.username
 
-    def checkPas(self, pas):
-        return self.password == pas
